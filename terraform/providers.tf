7a58eeda6a4c9613049fe8d17c0e0be9c8bed2e7
Stevi28 <stav.kourou19@gmail.com>
2026-04-06 02:35:47 +0300

    fix modules order 2

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -25,8 +25,6 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = true
     }
   }
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 provider "helm" {
