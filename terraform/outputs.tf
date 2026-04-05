1d1febf97474bc9d2b1a3b390ae6faebc57c3d44
Stevi28 <stav.kourou19@gmail.com>
2026-04-06 02:39:46 +0300

    fix names - outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,22 @@
 # -------------------------------------------------------
-# Resource Group
+# Resource Groups
 # -------------------------------------------------------
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = module.resource_group.name
+output "resource_group_net_name" {
+  description = "Name of the network resource group"
+  value       = module.resource_group.net_name
+}
+
+output "resource_group_data_name" {
+  description = "Name of the data resource group"
+  value       = module.resource_group.data_name
+}
+
+output "resource_group_app_name" {
+  description = "Name of the app resource group"
+  value       = module.resource_group.app_name
 }
 
 output "resource_group_location" {
-  description = "Location of the resource group"
+  description = "Location of the resource groups"
   value       = module.resource_group.location
 }
