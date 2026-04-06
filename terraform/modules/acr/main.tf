7843a020f68659af40e151f39718f8faafbec5b8
Stevi28 <stav.kourou19@gmail.com>
2026-04-06 16:48:23 +0300

    feat: add acr module with private endpoint and dns zone

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -16,29 +16,6 @@ resource "azurerm_container_registry" "main" {
 }
 
 # -------------------------------------------------------
-# Private Endpoint για ACR
-# -------------------------------------------------------
-resource "azurerm_private_endpoint" "acr" {
-  name                = "pe-acr-${var.project_name}-${var.environment}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.private_endpoint_subnet_id
-
-  private_service_connection {
-    name                           = "psc-acr-${var.project_name}-${var.environment}"
-    private_connection_resource_id = azurerm_container_registry.main.id
-    subresource_names              = ["registry"]
-    is_manual_connection           = false
-  }
-
-  tags = {
-    project     = var.project_name
-    environment = var.environment
-    managed_by  = "terraform"
-  }
-}
-
-# -------------------------------------------------------
 # Private DNS Zone για ACR
 # -------------------------------------------------------
 resource "azurerm_private_dns_zone" "acr" {
@@ -69,14 +46,31 @@ resource "azurerm_private_dns_zone_virtual_network_link" "acr" {
 }
 
 # -------------------------------------------------------
-# DNS Zone Group
+# Private Endpoint για ACR (με DNS Zone Group μέσα)
 # -------------------------------------------------------
-resource "azurerm_private_endpoint_dns_zone_group" "acr" {
-  name                 = "dzg-acr-${var.project_name}-${var.environment}"
-  private_endpoint_id  = azurerm_private_endpoint.acr.id
+resource "azurerm_private_endpoint" "acr" {
+  name                = "pe-acr-${var.project_name}-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
 
-  private_dns_zone_configs {
-    name                = "privatelink-azurecr-io"
-    private_dns_zone_id = azurerm_private_dns_zone.acr.id
+  private_service_connection {
+    name                           = "psc-acr-${var.project_name}-${var.environment}"
+    private_connection_resource_id = azurerm_container_registry.main.id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
   }
+
+  private_dns_zone_group {
+    name                 = "dzg-acr-${var.project_name}-${var.environment}"
+    private_dns_zone_ids = [azurerm_private_dns_zone.acr.id]
+  }
+
+  tags = {
+    project     = var.project_name
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.acr]
 }
